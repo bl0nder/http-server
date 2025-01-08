8b37e34d28cf40aed4a9c2c62a9eee2d678c213d
bl0nder <goelpalaash@gmail.com>
2025-01-08 20:10:01 +0530

    More descriptive error handling + SIGINT handled to explicitly close socket

--- http_server.c
+++ http_server.c
@@ -4,17 +4,25 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <inttypes.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
 
 #define LISTENING_ADDR "127.0.0.1"
 
 char* err_msg;
+char* err_loc;
+
+int server_socket_fd = -1;
 
 int create_server(int port, int backlog) {
 	
     //Create socket 
 	int s = socket(AF_INET, SOCK_STREAM, 0);
 	if (s < 0) {
-		err_msg = "Error in creating socket.";
+		err_msg = strerror(errno);
+        err_loc = "Server socket:socket()";
 		return -1;
 	}
 
@@ -26,35 +34,52 @@ int create_server(int port, int backlog) {
 	
 	// Bind socket to server 
 	if (bind(s, (const struct sockaddr*) &server, sizeof(server)) < 0) {
-		err_msg = "Error in binding socket.";
+        err_msg = strerror(errno);
+		err_loc = "Server socket:bind()";
 		return -1;	
 	}
 	
 	//Listens for connections
 	if (listen(s, backlog) < 0) {
-		err_msg = "Error in listening for connections.";
+        err_msg = strerror(errno);
+		err_loc = "Server socket:listen()";
 		return -1;
 	}
 
 	return s;
 }
 
+void sigint_handler(int sig) {
+    printf("SIGINT received. Exiting.\n", sig);
+
+    if (server_socket_fd >= 0) {
+        close(server_socket_fd);
+        printf("Socket has been closed.\n");
+    }
+    exit(0);
+}
+
 int main() {
+    
     //Variables
 	int port = 1234;
-    int backlog = 10;
+    int backlog = 2;
+    
+    //Signal handler
+    signal(SIGINT, sigint_handler);
 
-	int server_socket_fd = create_server(port, backlog);
+	server_socket_fd = create_server(port, backlog);
 	if (server_socket_fd < 0){
-		fprintf(stderr, err_msg);	
+		fprintf(stderr, "[!]%s in %s\n", err_msg, err_loc);	
         close(server_socket_fd);
 		return -1;
 	}
 	
 	printf("Listening on port %d...\n", port);
-	while(1) {
+
+    while(1) {
         //Put something here
-	}
+    }
 
 	return 0;
 }
